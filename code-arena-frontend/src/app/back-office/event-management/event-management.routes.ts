71d385367558c89b08733c3d3a04a5750632703b
Nesrine Zaiem <nesrine.zaiem@esprit.tn>
2026-03-28 16:54:34 +0100

    backoffice

--- code-arena-frontend/src/app/back-office/event-management/event-management.routes.ts
+++ code-arena-frontend/src/app/back-office/event-management/event-management.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { AdminEventListComponent } from './pages/admin-event-list/admin-event-list.component';
 import { AdminEventDetailComponent } from './pages/admin-event-detail/admin-event-detail.component';
-import { CreateEventComponent } from '../../front-office/event/pages/create-event/create-event.component';
+import { CreateEventComponent } from './pages/create-event/create-event.component';
 
 export const EVENT_MANAGEMENT_ROUTES: Routes = [
   { path: '', component: AdminEventListComponent },
   { path: 'create', component: CreateEventComponent },
   { path: ':id', component: AdminEventDetailComponent }
 ];
+
--- code-arena-frontend/src/app/front-office/event/event.routes.ts
+++ code-arena-frontend/src/app/front-office/event/event.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 import { EventListComponent } from './pages/event-list/event-list.component';
-import { CreateEventComponent } from './pages/create-event/create-event.component';
 import { EventDetailComponent } from './pages/event-detail/event-detail.component';
 import { MyRegistrationsComponent } from './pages/my-registrations/my-registrations.component';
 import { MyInvitationsComponent } from './pages/my-invitations/my-invitations.component';
 
 export const EVENT_ROUTES: Routes = [
   { path: '', component: EventListComponent },
-  { path: 'create', component: CreateEventComponent },
   { path: 'my-registrations', component: MyRegistrationsComponent },
   { path: 'my-invitations', component: MyInvitationsComponent },
   { path: ':id', component: EventDetailComponent },
