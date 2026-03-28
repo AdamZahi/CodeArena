816689d53a5d73228e8806d6a5ae5e697202ee52
Nesrine Zaiem <nesrine.zaiem@esprit.tn>
2026-03-28 20:18:12 +0100

    feat(module6): admin dashbord

--- code-arena-frontend/src/app/back-office/event-management/event-management.routes.ts
+++ code-arena-frontend/src/app/back-office/event-management/event-management.routes.ts
@@ -3,9 +3,12 @@ import { AdminEventListComponent } from './pages/admin-event-list/admin-event-li
 import { AdminEventDetailComponent } from './pages/admin-event-detail/admin-event-detail.component';
 import { CreateEventComponent } from './pages/create-event/create-event.component';
 
+import { AdminEventEditComponent } from './pages/admin-event-edit/admin-event-edit.component';
+
 export const EVENT_MANAGEMENT_ROUTES: Routes = [
   { path: '', component: AdminEventListComponent },
   { path: 'create', component: CreateEventComponent },
+  { path: ':id/edit', component: AdminEventEditComponent },
   { path: ':id', component: AdminEventDetailComponent }
 ];
 
