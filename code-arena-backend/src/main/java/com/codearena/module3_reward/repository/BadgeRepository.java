7b585e1fa4de4b51f95531edd5b35ab913c2d094
Adam zahi <zahi.adem10@gmail.com>
2026-04-01 07:52:13 +0100

    feat: Add custom query method findByCriteria to BadgeRepository

--- code-arena-backend/src/main/java/com/codearena/module3_reward/repository/BadgeRepository.java
+++ code-arena-backend/src/main/java/com/codearena/module3_reward/repository/BadgeRepository.java
@@ -3,8 +3,10 @@ package com.codearena.module3_reward.repository;
 import com.codearena.module3_reward.entity.Badge;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface BadgeRepository extends JpaRepository<Badge, UUID> {
-    // TODO: Add custom query methods.
+
+    Optional<Badge> findByCriteria(String criteria);
 }
