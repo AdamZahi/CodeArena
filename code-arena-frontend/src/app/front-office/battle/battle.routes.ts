b52d880cc068be05f68618e4d02a63ecb4513fcb
Adam zahi <zahi.adem10@gmail.com>
2026-04-03 11:21:02 +0100

    feat: Refactor ApiService to improve base URL resolution and update BATTLE_ROUTES to load the correct initial component

--- code-arena-frontend/src/app/front-office/battle/battle.routes.ts
+++ code-arena-frontend/src/app/front-office/battle/battle.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const BATTLE_ROUTES: Routes = [
-  { path: '', loadComponent: () => import('./pages/battle-lobby/battle-lobby.component').then((m) => m.BattleLobbyComponent) },
+  { path: '', loadComponent: () => import('./pages/battle-list/battle-list.component').then((m) => m.BattleListComponent) },
   { path: 'lobby/:roomId', loadComponent: () => import('./pages/battle-lobby/battle-lobby.component').then((m) => m.BattleLobbyComponent) },
   { path: 'room/:roomId', loadComponent: () => import('./pages/battle-room/battle-room.component').then((m) => m.BattleRoomComponent) },
   { path: 'result/:roomId', loadComponent: () => import('./pages/battle-result/battle-result.component').then((m) => m.BattleResultComponent) }
