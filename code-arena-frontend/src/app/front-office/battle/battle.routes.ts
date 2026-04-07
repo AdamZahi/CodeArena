33d52adb308d657a2ecc548a04c34cfa139e395d
Adam zahi <zahi.adem10@gmail.com>
2026-04-07 14:10:13 +0100

    feat: add route for sharing battle results with token

--- code-arena-frontend/src/app/front-office/battle/battle.routes.ts
+++ code-arena-frontend/src/app/front-office/battle/battle.routes.ts
@@ -4,5 +4,6 @@ export const BATTLE_ROUTES: Routes = [
   { path: '', loadComponent: () => import('./pages/battle-list/battle-list.component').then((m) => m.BattleListComponent) },
   { path: 'lobby/:roomId', loadComponent: () => import('./pages/battle-lobby/battle-lobby.component').then((m) => m.BattleLobbyComponent) },
   { path: 'room/:roomId', loadComponent: () => import('./pages/battle-room/battle-room.component').then((m) => m.BattleRoomComponent) },
-  { path: 'result/:roomId', loadComponent: () => import('./pages/battle-result/battle-result.component').then((m) => m.BattleResultComponent) }
+  { path: 'result/:roomId', loadComponent: () => import('./pages/battle-result/battle-result.component').then((m) => m.BattleResultComponent) },
+  { path: 'share/:token', loadComponent: () => import('./pages/battle-share/battle-share.component').then((m) => m.BattleShareComponent) }
 ];
